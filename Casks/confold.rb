cd90154b4a8128b74ccd68626ac8b4232e9833d8
Juan Garcia Longaron <juan.mobilife@gmail.com>
2026-07-14 08:11:05 +0000

    confold 0.6.0: update cask

--- Casks/confold.rb
+++ Casks/confold.rb
@@ -1,14 +1,14 @@
 cask "confold" do
   # Update version + both sha256 values on each release (use scripts/bump-packaging.sh).
-  version "0.5.1"
+  version "0.6.0"
 
   on_arm do
-    sha256 "47dc3f27bb4c999c4a7eb7f53a7516355029705667f6311c042913744801e7a5"
+    sha256 "60a9f7916e2ea86393523f348c7cbf6114dcfb7695507fb86687495468b8708e"
     url "https://github.com/confold/confold/releases/download/v#{version}/Confold_#{version}_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "f6a7baad1ded7ccaf3f875a6325def6e7e59410a68df894e8933b71f6fd88fe3"
+    sha256 "bf627b1cef29fc66be52713394bbeabe75293cc392aec18ea15cad0d4103be7e"
     url "https://github.com/confold/confold/releases/download/v#{version}/Confold_#{version}_x64.dmg"
   end
 
